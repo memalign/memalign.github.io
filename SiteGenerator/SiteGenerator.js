406fe6a81ed5d973134ea080cf13c0aac41cf7c1
memalign <memalign@not-a-valid-address.com>
2019-12-26 02:57:09 -0800

    Starting to flesh out entry format, added unit test harness to SiteGenerator

--- SiteGenerator/SiteGenerator.js
+++ SiteGenerator/SiteGenerator.js
@@ -1,33 +1,75 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: orange; icon-glyph: laptop-code;
+
+const UNIT_TEST = false
+
+
+// Entry class
+
+class Entry {
+   constructor(filename, fileContents /* optional */) {
+  
+    this.filename = filename
+    
+    if (/DRAFT-(.+).txt/.test(filename)) {
+      this.isDraft = true
+      return this
+    }
+    
+    let matches = this.filename.match(/\/(\d+)-(.+).txt/)
+    
+    this.postNumber = parseInt(matches[1])
+    this.postLinkName = matches[2] // e.g. "this-website"
+    
+    let contents = fileContents ? fileContents : this.fileContents()
+    
+    let contentsMatches = contents.match(/^Title: ([^\r\n]+)[\r\n]+Date: (\d+\/\d+\/\d+)[\r\n]+(.+)$/ms)
+    
+    this.title = contentsMatches[1]
+    this.dateString = contentsMatches[2]
+    this.contents = contentsMatches[3]
+  }
+  
+  fileContents() {
+    return FileManager.local().readString(this.filename)
+  }  
+}
+
+
 // Utilities
 
 function currentScriptFilename() {
-  return Script.name() + ".js";
+  return Script.name() + ".js"
 }
 
 function currentScriptPath() {
-  return FileManager.iCloud().documentsDirectory() + "/" + currentScriptFilename();
+  return FileManager.iCloud().documentsDirectory() + "/" + currentScriptFilename()
 }
 
 function repoPath() {
   return FileManager.local().bookmarkedPath("memalign.github.io")
 }
 
+function entriesPath() {
+  return repoPath() + "/entries"
+}
+
 function repoSiteGeneratorPath() {
-   return repoPath() + "/SiteGenerator/"; 
+   return repoPath() + "/SiteGenerator"
 }
 
 function copyCurrentScriptToRepo() {
   let fm = FileManager.local()
-  let destPath = repoSiteGeneratorPath() + currentScriptFilename()
+  let destPath = repoSiteGeneratorPath() + "/" + currentScriptFilename()
   if (fm.fileExists(destPath)) {
     fm.remove(destPath)
   }
   fm.copy(currentScriptPath(), destPath)
 }
 
+
+// runScript
 
 function runScript() {
   console.log("=> Backing up script")
@@ -36,14 +78,68 @@ function runScript() {
   
   let fm = FileManager.local()
 
-  let entryFilePaths = fm.listContents(repoPath() + "/entries")
+  let entryFilenames = fm.listContents(entriesPath())
+  for (entryFilename of entryFilenames) {
+    console.log("Entry filename: " + entryFilename)
+    let entry = new Entry(entriesPath() + "/" + entryFilename)
+    if (entry.isDraft) {
+      continue
+    }
+    console.log("=== Post #" + entry.postNumber + " (" + entry.postLinkName + ") ===")
+    console.log(entry.fileContents())
+    console.log("===")
+  }
+
         
-  console.log("Entry paths: " + entryFilePaths)
-        
-  let outputPath = repoPath() + "/output.txt"
-        
-  fm.writeString(outputPath, "test")          
+//  let outputPath = repoPath() + "/output.txt"      
+//  fm.writeString(outputPath, "test")          
 }
 
-runScript()
+if (!UNIT_TEST) {
+  runScript()
+  return
+}
+
+
+
+// Unit tests
+
+function assertTrue(condition, str) {
+    if (!condition) {
+      throw "Failed assertion: " + str
+    }
+}
+
+class UnitTests {
+  //TODO: Add unit tests for parsing a draft Entry, a non-draft Entry
+  
+  test_1() {
+    console.log("1")
+    assertTrue(true, "1 is true")
+  }
+  
+  test_2() {
+    console.log("2")
+  } 
+
+            
+  run() {
+    let methods = Object.getOwnPropertyNames(Object.getPrototypeOf(this))
+    
+    for (let method of methods) {
+      if (method.startsWith("test")) {
+        console.log("=== Invoking " + method + " ===")
+        this[method]();
+        console.log("======")
+      }
+    }
+  }
+}
+
+function runUnitTests() {
+  let ut = new UnitTests()
+  ut.run()
+}
+  
+runUnitTests()
 
