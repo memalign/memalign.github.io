161f0c5da4b66cc199cf8126fd2d5011697018f2
memalign <memalign@not-a-valid-address.com>
2022-11-24 20:40:28 -0800

    Unit test coverage for negative entriesOnIndex

--- SiteGenerator/SiteGenerator.js
+++ SiteGenerator/SiteGenerator.js
@@ -2,7 +2,7 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: orange; icon-glyph: laptop-code;
 
-const UNIT_TEST = true
+const UNIT_TEST = false
 
 
 // Utilities
@@ -1410,6 +1410,301 @@ More posts:<br />
 `
 
     assertTrue(index.toHTML() == expectation, "index html")
+  }
+  
+  test_Index_toHTML_negativeEntriesOnIndex() {
+    let siteConfig = new SiteConfig("/path/to/repo", `{
+  "title":"memalign.github.io",
+  "baseURL":"https://memalign.github.io",
+  "entriesOnIndex":-1,
+  "authorName":"memalign",
+  "authorURL":"https://twitter.com/memalign"
+}    
+`)
+    
+    let entry1 = new Entry(siteConfig, "/path/0001-some-title.txt", "Title: This title\nDate: 12/26/2019\nTags: Tag1\n[Image:/m/test1.jpg]\ntext1 text1")
+    let entry2 = new Entry(siteConfig, "/path/0002-some-title2.txt", "Title: This title2\nDate: 12/27/2019\nTags: Tag2\ntext2 text2")
+    let entry3 = new Entry(siteConfig, "/path/0003-some-title3.txt", "Title: This title3\nDate: 12/28/2019\nTags: iTag, iTag2\n[Image:/m/test3.jpg]\ntext3 text3")
+    let entry4 = new Entry(siteConfig, "/path/0004-some-title4.txt", "Title: This title4\nDate: 12/28/2019\nTags: iTag, iTag2\n[Image:/m/test3.jpg]\ntext3 text3")
+    let entry5 = new Entry(siteConfig, "/path/0005-some-title5.txt", "Title: This title5\nDate: 12/28/2019\nTags: iTag, iTag2\n[Image:/m/test3.jpg]\ntext3 text3")
+    let entry6 = new Entry(siteConfig, "/path/0006-some-title6.txt", "Title: This title6\nDate: 12/28/2019\nTags: iTag, iTag2\n[Image:/m/test3.jpg]\ntext3 text3")
+    let entry7 = new Entry(siteConfig, "/path/0007-some-title7.txt", "Title: This title7\nDate: 12/28/2019\nTags: iTag, iTag2\n[Image:/m/test3.jpg]\ntext3 text3")
+    let entry8 = new Entry(siteConfig, "/path/0008-some-title8.txt", "Title: This title8\nDate: 12/28/2019\nTags: iTag, iTag2\n[Image:/m/test3.jpg]\ntext3 text3")
+    let entry9 = new Entry(siteConfig, "/path/0009-some-title9.txt", "Title: This title9\nDate: 12/28/2019\nTags: iTag, iTag2\n[Image:/m/test3.jpg]\ntext3 text3")
+    let entry10 = new Entry(siteConfig, "/path/0010-some-title10.txt", "Title: This title10\nDate: 12/28/2019\nTags: iTag, iTag2\n[Image:/m/test3.jpg]\ntext3 text3")
+    let entry11 = new Entry(siteConfig, "/path/0011-some-title11.txt", "Title: This title11\nDate: 12/28/2019\nTags: iTag, iTag2\n[Image:/m/test3.jpg]\ntext3 text3")
+    let entry12 = new Entry(siteConfig, "/path/0012-some-title12.txt", "Title: This title12P\nDate: 12/28/2019\nTags: iTag, iTag2, Programming\n[Image:/m/test3.jpg]\ntext3 text3")
+    let entry13 = new Entry(siteConfig, "/path/0013-some-title13.txt", "Title: This title13L\nDate: 12/28/2019\nTags: iTag, iTag2, Leisure\n[Image:/m/test3.jpg]\ntext3 text3")
+    let entry14 = new Entry(siteConfig, "/path/0014-some-title14.txt", "Title: This title14IF\nDate: 12/28/2019\nTags: iTag, iTag2, Interactive Fiction\n[Image:/m/test3.jpg]\ntext3 text3")
+    let entry15 = new Entry(siteConfig, "/path/0015-some-title15.txt", "Title: This title15IF\nDate: 12/28/2019\nTags: Interactive Fiction\n[Image:/m/test3.jpg]\ntext3 text3")
+    let entry16 = new Entry(siteConfig, "/path/0016-some-title16.txt", "Title: This title16IF\nDate: 11/24/2022\nTags: Interactive Fiction\n[Image:/m/test3.jpg]\ntext3 text3")
+
+    
+    let index = new Index(siteConfig, [entry2, entry1, entry3, entry4, entry5, entry6, entry7, entry8, entry9, entry10, entry11, entry12, entry13, entry14, entry15, entry16])  
+    
+    let expectation = `<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="alternate" type="application/json" href="/feed.json" />
+<link rel="alternate" type="application/atom+xml" href="/feed.xml" />
+<title>memalign.github.io</title>
+<meta property="og:title" content="memalign.github.io" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://memalign.github.io/index.html" />
+<meta property="og:image" content="https://memalign.github.io/m/test3.jpg" />
+<meta property="og:description" content="text3 text3" />
+<link rel="stylesheet" href="/style.css">
+<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0, user-scalable=yes'>
+</head>
+<body>
+<div id="body">
+<div id='feeds'>
+<a href='/feed.xml'>RSS</a> | <a href='/feed.json'>JSON Feed</a>
+</div><div id='header'><h1>memalign.github.io</h1></div>
+<div id='allposts'>
+<div id='grid'>
+<div id='grid-entry'>
+<a href='p/some-title16.html'>This title16IF</a><br />
+<a href='p/some-title15.html'>This title15IF</a><br />
+<a href='p/some-title14.html'>This title14IF</a><br />
+<a href='p/some-title13.html'>This title13L</a><br />
+<a href='p/some-title12.html'>This title12P</a><br />
+<a href='p/some-title11.html'>This title11</a><br />
+</div><div id='grid-entry'>
+<a href='p/some-title10.html'>This title10</a><br />
+<a href='p/some-title9.html'>This title9</a><br />
+<a href='p/some-title8.html'>This title8</a><br />
+<a href='p/some-title7.html'>This title7</a><br />
+<a href='p/some-title6.html'>This title6</a><br />
+<a href='p/some-title5.html'>This title5</a><br />
+</div><div id='grid-entry'>
+<a href='p/some-title4.html'>This title4</a><br />
+<a href='p/some-title3.html'>This title3</a><br />
+<a href='p/some-title2.html'>This title2</a><br />
+<a href='p/some-title.html'>This title</a><br />
+</div>
+</div>
+</div>
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title16.html'>This title16IF</a>
+</h2>
+</div>
+<img src="/m/test3.jpg">
+<div id='postdate'>Posted on 11/24/2022<br />
+Tags: <a href='/tags.html'>Interactive Fiction</a></div>
+text3 text3
+</div>
+
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title15.html'>This title15IF</a>
+</h2>
+</div>
+<img src="/m/test3.jpg">
+<div id='postdate'>Posted on 12/28/2019<br />
+Tags: <a href='/tags.html'>Interactive Fiction</a></div>
+text3 text3
+</div>
+
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title14.html'>This title14IF</a>
+</h2>
+</div>
+<img src="/m/test3.jpg">
+<div id='postdate'>Posted on 12/28/2019<br />
+Tags: <a href='/tags.html'>iTag</a>, <a href='/tags.html'>iTag2</a>, <a href='/tags.html'>Interactive Fiction</a></div>
+text3 text3
+</div>
+
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title13.html'>This title13L</a>
+</h2>
+</div>
+<img src="/m/test3.jpg">
+<div id='postdate'>Posted on 12/28/2019<br />
+Tags: <a href='/tags.html'>iTag</a>, <a href='/tags.html'>iTag2</a>, <a href='/tags.html'>Leisure</a></div>
+text3 text3
+</div>
+
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title12.html'>This title12P</a>
+</h2>
+</div>
+<img src="/m/test3.jpg">
+<div id='postdate'>Posted on 12/28/2019<br />
+Tags: <a href='/tags.html'>iTag</a>, <a href='/tags.html'>iTag2</a>, <a href='/tags.html'>Programming</a></div>
+text3 text3
+</div>
+
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title11.html'>This title11</a>
+</h2>
+</div>
+<img src="/m/test3.jpg">
+<div id='postdate'>Posted on 12/28/2019<br />
+Tags: <a href='/tags.html'>iTag</a>, <a href='/tags.html'>iTag2</a></div>
+text3 text3
+</div>
+
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title10.html'>This title10</a>
+</h2>
+</div>
+<img src="/m/test3.jpg">
+<div id='postdate'>Posted on 12/28/2019<br />
+Tags: <a href='/tags.html'>iTag</a>, <a href='/tags.html'>iTag2</a></div>
+text3 text3
+</div>
+
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title9.html'>This title9</a>
+</h2>
+</div>
+<img src="/m/test3.jpg">
+<div id='postdate'>Posted on 12/28/2019<br />
+Tags: <a href='/tags.html'>iTag</a>, <a href='/tags.html'>iTag2</a></div>
+text3 text3
+</div>
+
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title8.html'>This title8</a>
+</h2>
+</div>
+<img src="/m/test3.jpg">
+<div id='postdate'>Posted on 12/28/2019<br />
+Tags: <a href='/tags.html'>iTag</a>, <a href='/tags.html'>iTag2</a></div>
+text3 text3
+</div>
+
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title7.html'>This title7</a>
+</h2>
+</div>
+<img src="/m/test3.jpg">
+<div id='postdate'>Posted on 12/28/2019<br />
+Tags: <a href='/tags.html'>iTag</a>, <a href='/tags.html'>iTag2</a></div>
+text3 text3
+</div>
+
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title6.html'>This title6</a>
+</h2>
+</div>
+<img src="/m/test3.jpg">
+<div id='postdate'>Posted on 12/28/2019<br />
+Tags: <a href='/tags.html'>iTag</a>, <a href='/tags.html'>iTag2</a></div>
+text3 text3
+</div>
+
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title5.html'>This title5</a>
+</h2>
+</div>
+<img src="/m/test3.jpg">
+<div id='postdate'>Posted on 12/28/2019<br />
+Tags: <a href='/tags.html'>iTag</a>, <a href='/tags.html'>iTag2</a></div>
+text3 text3
+</div>
+
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title4.html'>This title4</a>
+</h2>
+</div>
+<img src="/m/test3.jpg">
+<div id='postdate'>Posted on 12/28/2019<br />
+Tags: <a href='/tags.html'>iTag</a>, <a href='/tags.html'>iTag2</a></div>
+text3 text3
+</div>
+
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title3.html'>This title3</a>
+</h2>
+</div>
+<img src="/m/test3.jpg">
+<div id='postdate'>Posted on 12/28/2019<br />
+Tags: <a href='/tags.html'>iTag</a>, <a href='/tags.html'>iTag2</a></div>
+text3 text3
+</div>
+
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title2.html'>This title2</a>
+</h2>
+</div>
+<div id='postdate'>Posted on 12/27/2019<br />
+Tags: <a href='/tags.html'>Tag2</a></div>
+text2 text2
+</div>
+
+<hr />
+<div id='post'>
+<div id='header'>
+<h2>
+<a href='p/some-title.html'>This title</a>
+</h2>
+</div>
+<img src="/m/test1.jpg">
+<div id='postdate'>Posted on 12/26/2019<br />
+Tags: <a href='/tags.html'>Tag1</a></div>
+text1 text1
+</div>
+
+
+</div>
+<div id="footer"></div>
+</body>
+</html>
+`
+
+    assertEqual(index.toHTML(), expectation)
   }
   
   test_Index_toHTML_enoughPostsToListInColumns_featured() {
