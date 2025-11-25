56d8fd60218ba7f4fcb24a6e4174995b2fb6e302
memalign <memalign@users.noreply.github.com>
2025-11-25 02:45:26 -0800

    TapToy: Offline PWA support

---
+++ m/taptoy/sw.js
@@ -0,0 +1,164 @@
+const cacheName = 'taptoy-ed97fd963114a6408bcdeb06ca810207';
+const appShellFiles = [
+  './BGColorAnimator.js',
+  './Images.js',
+  './PCEImage.js',
+  './PWAImages/android/android-launchericon-144-144.png',
+  './PWAImages/android/android-launchericon-192-192.png',
+  './PWAImages/android/android-launchericon-48-48.png',
+  './PWAImages/android/android-launchericon-512-512.png',
+  './PWAImages/android/android-launchericon-72-72.png',
+  './PWAImages/android/android-launchericon-96-96.png',
+  './PWAImages/ios/100.png',
+  './PWAImages/ios/1024.png',
+  './PWAImages/ios/114.png',
+  './PWAImages/ios/120.png',
+  './PWAImages/ios/128.png',
+  './PWAImages/ios/144.png',
+  './PWAImages/ios/152.png',
+  './PWAImages/ios/16.png',
+  './PWAImages/ios/167.png',
+  './PWAImages/ios/180.png',
+  './PWAImages/ios/192.png',
+  './PWAImages/ios/20.png',
+  './PWAImages/ios/256.png',
+  './PWAImages/ios/29.png',
+  './PWAImages/ios/32.png',
+  './PWAImages/ios/40.png',
+  './PWAImages/ios/50.png',
+  './PWAImages/ios/512.png',
+  './PWAImages/ios/57.png',
+  './PWAImages/ios/58.png',
+  './PWAImages/ios/60.png',
+  './PWAImages/ios/64.png',
+  './PWAImages/ios/72.png',
+  './PWAImages/ios/76.png',
+  './PWAImages/ios/80.png',
+  './PWAImages/ios/87.png',
+  './PWAImages/windows11/LargeTile.scale-100.png',
+  './PWAImages/windows11/LargeTile.scale-125.png',
+  './PWAImages/windows11/LargeTile.scale-150.png',
+  './PWAImages/windows11/LargeTile.scale-200.png',
+  './PWAImages/windows11/LargeTile.scale-400.png',
+  './PWAImages/windows11/SmallTile.scale-100.png',
+  './PWAImages/windows11/SmallTile.scale-125.png',
+  './PWAImages/windows11/SmallTile.scale-150.png',
+  './PWAImages/windows11/SmallTile.scale-200.png',
+  './PWAImages/windows11/SmallTile.scale-400.png',
+  './PWAImages/windows11/SplashScreen.scale-100.png',
+  './PWAImages/windows11/SplashScreen.scale-125.png',
+  './PWAImages/windows11/SplashScreen.scale-150.png',
+  './PWAImages/windows11/SplashScreen.scale-200.png',
+  './PWAImages/windows11/SplashScreen.scale-400.png',
+  './PWAImages/windows11/Square150x150Logo.scale-100.png',
+  './PWAImages/windows11/Square150x150Logo.scale-125.png',
+  './PWAImages/windows11/Square150x150Logo.scale-150.png',
+  './PWAImages/windows11/Square150x150Logo.scale-200.png',
+  './PWAImages/windows11/Square150x150Logo.scale-400.png',
+  './PWAImages/windows11/Square44x44Logo.altform-lightunplated_targetsize-16.png',
+  './PWAImages/windows11/Square44x44Logo.altform-lightunplated_targetsize-20.png',
+  './PWAImages/windows11/Square44x44Logo.altform-lightunplated_targetsize-24.png',
+  './PWAImages/windows11/Square44x44Logo.altform-lightunplated_targetsize-256.png',
+  './PWAImages/windows11/Square44x44Logo.altform-lightunplated_targetsize-30.png',
+  './PWAImages/windows11/Square44x44Logo.altform-lightunplated_targetsize-32.png',
+  './PWAImages/windows11/Square44x44Logo.altform-lightunplated_targetsize-36.png',
+  './PWAImages/windows11/Square44x44Logo.altform-lightunplated_targetsize-40.png',
+  './PWAImages/windows11/Square44x44Logo.altform-lightunplated_targetsize-44.png',
+  './PWAImages/windows11/Square44x44Logo.altform-lightunplated_targetsize-48.png',
+  './PWAImages/windows11/Square44x44Logo.altform-lightunplated_targetsize-60.png',
+  './PWAImages/windows11/Square44x44Logo.altform-lightunplated_targetsize-64.png',
+  './PWAImages/windows11/Square44x44Logo.altform-lightunplated_targetsize-72.png',
+  './PWAImages/windows11/Square44x44Logo.altform-lightunplated_targetsize-80.png',
+  './PWAImages/windows11/Square44x44Logo.altform-lightunplated_targetsize-96.png',
+  './PWAImages/windows11/Square44x44Logo.altform-unplated_targetsize-16.png',
+  './PWAImages/windows11/Square44x44Logo.altform-unplated_targetsize-20.png',
+  './PWAImages/windows11/Square44x44Logo.altform-unplated_targetsize-24.png',
+  './PWAImages/windows11/Square44x44Logo.altform-unplated_targetsize-256.png',
+  './PWAImages/windows11/Square44x44Logo.altform-unplated_targetsize-30.png',
+  './PWAImages/windows11/Square44x44Logo.altform-unplated_targetsize-32.png',
+  './PWAImages/windows11/Square44x44Logo.altform-unplated_targetsize-36.png',
+  './PWAImages/windows11/Square44x44Logo.altform-unplated_targetsize-40.png',
+  './PWAImages/windows11/Square44x44Logo.altform-unplated_targetsize-44.png',
+  './PWAImages/windows11/Square44x44Logo.altform-unplated_targetsize-48.png',
+  './PWAImages/windows11/Square44x44Logo.altform-unplated_targetsize-60.png',
+  './PWAImages/windows11/Square44x44Logo.altform-unplated_targetsize-64.png',
+  './PWAImages/windows11/Square44x44Logo.altform-unplated_targetsize-72.png',
+  './PWAImages/windows11/Square44x44Logo.altform-unplated_targetsize-80.png',
+  './PWAImages/windows11/Square44x44Logo.altform-unplated_targetsize-96.png',
+  './PWAImages/windows11/Square44x44Logo.scale-100.png',
+  './PWAImages/windows11/Square44x44Logo.scale-125.png',
+  './PWAImages/windows11/Square44x44Logo.scale-150.png',
+  './PWAImages/windows11/Square44x44Logo.scale-200.png',
+  './PWAImages/windows11/Square44x44Logo.scale-400.png',
+  './PWAImages/windows11/Square44x44Logo.targetsize-16.png',
+  './PWAImages/windows11/Square44x44Logo.targetsize-20.png',
+  './PWAImages/windows11/Square44x44Logo.targetsize-24.png',
+  './PWAImages/windows11/Square44x44Logo.targetsize-256.png',
+  './PWAImages/windows11/Square44x44Logo.targetsize-30.png',
+  './PWAImages/windows11/Square44x44Logo.targetsize-32.png',
+  './PWAImages/windows11/Square44x44Logo.targetsize-36.png',
+  './PWAImages/windows11/Square44x44Logo.targetsize-40.png',
+  './PWAImages/windows11/Square44x44Logo.targetsize-44.png',
+  './PWAImages/windows11/Square44x44Logo.targetsize-48.png',
+  './PWAImages/windows11/Square44x44Logo.targetsize-60.png',
+  './PWAImages/windows11/Square44x44Logo.targetsize-64.png',
+  './PWAImages/windows11/Square44x44Logo.targetsize-72.png',
+  './PWAImages/windows11/Square44x44Logo.targetsize-80.png',
+  './PWAImages/windows11/Square44x44Logo.targetsize-96.png',
+  './PWAImages/windows11/StoreLogo.scale-100.png',
+  './PWAImages/windows11/StoreLogo.scale-125.png',
+  './PWAImages/windows11/StoreLogo.scale-150.png',
+  './PWAImages/windows11/StoreLogo.scale-200.png',
+  './PWAImages/windows11/StoreLogo.scale-400.png',
+  './PWAImages/windows11/Wide310x150Logo.scale-100.png',
+  './PWAImages/windows11/Wide310x150Logo.scale-125.png',
+  './PWAImages/windows11/Wide310x150Logo.scale-150.png',
+  './PWAImages/windows11/Wide310x150Logo.scale-200.png',
+  './PWAImages/windows11/Wide310x150Logo.scale-400.png',
+  './SoundEffects.js',
+  './Utilities.js',
+  './bundle.js',
+  './drip.mp3',
+  './icon-2100x2100.png',
+  './index.html',
+  './main.css',
+  './manifest.json',
+  './songs/06-Meydan-LEtoile-danse-Pt.-1.mp3',
+  './songs/Funny-Bubbles-Happy-Classical-Music.mp3',
+  './songs/Kimiko_Ishizaka_-_05_-_Variatio_4_a_1_Clav.mp3',
+  './songs/Kimiko_Ishizaka_-_07_-_Variatio_6_a_1_Clav_Canone_alla_Seconda.mp3',
+  './songs/Orchestral-Suite-no.-2-in-B-minor-BWV-1067-7.-Badinerie.mp3',
+  './songs/README.txt',
+  './taptoy.jpg',
+  './taptoy.js',
+  './type.mp3',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(appShellFiles);
+  })());
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    for (const key of keys) {
+      if (key !== cacheName) {
+        await caches.delete(key);
+      }
+    }
+  })());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
